834eca586a5151a4f0bd850c8480d3dafe5410b3
Silbye <silverxlxl9@gmail.com>
2022-08-05 21:04:13 +0300

    optimized week days programm

--- lesson1/task2/Program.cs
+++ lesson1/task2/Program.cs
@@ -1,34 +1,31 @@
 ﻿Console.Write("Enter week day: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number == 0)
-{
-    Console.WriteLine("Today is Monday");
-}
-if (number == 1)
-{
-    Console.WriteLine("Today is Tuesday");
-}
-if (number == 2)
-{
-    Console.WriteLine("Today is Wednesday");
-}
-if (number == 3)
-{
-    Console.WriteLine("Today is Thursday");
-}
-if (number == 4)
-{
-    Console.WriteLine("Today is Friday");
-}
-if (number == 5)
-{
-    Console.WriteLine("Today is Saturday");
-}
-if (number == 6)
-{
-    Console.WriteLine("Today is Sunday");
-}
+
 if (number > 6)
 {
     Console.WriteLine("There is no such week day!");
+}
+
+switch(number){
+    case 0:
+    Console.WriteLine("Today is Monday");
+    break;
+    case 1:
+    Console.WriteLine("Today is Tuesday");
+    break;
+    case 2:
+    Console.WriteLine("Today is Wednesday");
+    break;
+    case 3:
+    Console.WriteLine("Today is Thursday");
+    break;
+    case 4:
+    Console.WriteLine("Today is Friday");
+    break;
+    case 5:
+    Console.WriteLine("Today is Saturday");
+    break;
+    case 6:
+    Console.WriteLine("Today is Sunday");
+    break;
 }
