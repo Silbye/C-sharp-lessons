36889bf8a9b39d6962e2bdc874e3186945539510
Silbye <silverxlxl9@gmail.com>
2022-08-05 20:23:55 +0300

    task 1 done

---
+++ lesson1/task1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Number is: " + number);
+int square = number * number;
+Console.WriteLine("Number's square is: " + square);
