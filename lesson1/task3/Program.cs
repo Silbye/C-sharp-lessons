77c8717d8d31cd88f7f090bb7c2f42f1ccef10a2
Silbye <silverxlxl9@gmail.com>
2022-08-05 21:28:20 +0300

    done number writing from negative n to positive n

---
+++ lesson1/task3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter your number: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int number = -n;
+
+while (number <= n){
+    Console.Write(number);
+        Console.Write(", ");
+        number++;
+}
+
+Console.WriteLine(" ");
+
+for(int i=-n; i<=n; i++){
+    Console.Write(i + ", ");
+}
