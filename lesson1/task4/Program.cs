953fceae0ca1b6a1dd6721e09c1f67300ea144ff
Silbye <silverxlxl9@gmail.com>
2022-08-05 21:35:43 +0300

    added final letter of number searcher

---
+++ lesson1/task4/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter your number: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int finalLetter = n%10;
+
+Console.WriteLine("Final letter of your number is " + finalLetter);
