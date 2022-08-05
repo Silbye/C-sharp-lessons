9bd6a620511ce4b7b8c217b9e0a22e8a961ea6fe
Silbye <silverxlxl9@gmail.com>
2022-08-05 20:37:18 +0300

    added square checking

--- lesson1/task1/Program.cs
+++ lesson1/task1/Program.cs
@@ -1,5 +1,18 @@
-﻿Console.WriteLine("Enter your number: ");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Number is: " + number);
-int square = number * number;
-Console.WriteLine("Number's square is: " + square);
+﻿Console.Write("Enter number 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Number 1 is " + number1);
+
+Console.Write("Enter number 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Number 2 is " + number2);
+
+int square = number1 * number1;
+Console.WriteLine("Number 1's square is " + square);
+if (square == number2)
+{
+    Console.WriteLine("Number 2 is Number 1's square");
+}
+else
+{
+    Console.WriteLine("Number 2 is NOT Number 1's square");
+}
