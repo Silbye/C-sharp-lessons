8ae68f55c9735922e357a4a96d1ef17981aff2cb
Silbye <silverxlxl9@gmail.com>
2022-08-07 05:13:03 +0300

    added even number check

---
+++ lesson1/hometasks/task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter your number: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+if(n%2 == 0)
+{
+    Console.Write("Number is even");
+}
+else
+{
+    Console.Write("Number is uneven");
+}
