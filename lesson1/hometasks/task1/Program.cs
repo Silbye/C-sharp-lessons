989196df12927c166072617df8dd10d9e7a1a7e2
Silbye <silverxlxl9@gmail.com>
2022-08-07 05:00:17 +0300

    added max number finder

---
+++ lesson1/hometasks/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Enter number 1: ");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter number 2: ");
+
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if(number1 > number2)
+{
+    max = number1;
+    Console.Write("Max number is " + number1);
+}
+else
+{
+    max = number2;
+    Console.Write("Max number is " + number2);
+}
