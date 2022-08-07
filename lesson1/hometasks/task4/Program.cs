92a0e44c633287448215269a1e4e9c3bc3493c7c
Silbye <silverxlxl9@gmail.com>
2022-08-07 05:17:36 +0300

    added even number count untill n

---
+++ lesson1/hometasks/task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter your number: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int number = 1;
+
+while (number <= n){
+    if(number % 2 == 0)
+    {
+        Console.Write(number);
+        Console.Write(", ");
+    }
+    number++;
+}
