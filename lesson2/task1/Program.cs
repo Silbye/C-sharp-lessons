cc77bd8255a008dae8a2d05489e4e3934a636499
Silbye <silverxlxl9@gmail.com>
2022-08-09 20:47:51 +0300

    finding max number of a number method

---
+++ lesson2/task1/Program.cs
@@ -0,0 +1,39 @@
+﻿int getMaxNumberRandomValue(){
+    int randomValue = new Random().Next(10, 100);
+
+    Console.WriteLine("The number is - " + randomValue);
+
+    int firstNumber = randomValue / 10;
+    int secondNumber = randomValue % 10;
+
+    int maxNumber = 0;
+    
+    if (firstNumber == secondNumber)
+    {
+        Console.WriteLine("The numbers are equal");
+    }
+    else
+    {
+        if (firstNumber > secondNumber)
+        {
+            maxNumber = firstNumber;
+        }
+        else
+        {
+            maxNumber = secondNumber;
+        }
+        Console.WriteLine("Max = " + maxNumber);
+    }
+    return maxNumber;
+}
+
+int numbrer = getMaxNumberRandomValue();
+Console.WriteLine(numbrer);
+
+
+
+
+
+
+
+
