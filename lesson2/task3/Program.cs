b71e4f0db2f972ab9ee8a77b0ef35e8aca62bf4b
Silbye <silverxlxl9@gmail.com>
2022-08-09 21:59:34 +0300

    added multiple finder

---
+++ lesson2/task3/Program.cs
@@ -0,0 +1,21 @@
+﻿void multiple()
+{
+    Console.Clear();
+
+    Console.Write("Enter Number 1: ");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter Number 1: ");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+
+    int remainder = number1 % number2;
+
+    if(remainder == 0)
+    {
+        Console.WriteLine("Number 1 is multiple of Number 2");
+    }
+    else
+    {
+        Console.WriteLine("Number 1 is not multiple of Number 2, and the remainder is - " + remainder);
+    }
+}
+multiple();
