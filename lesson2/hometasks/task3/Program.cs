b849839b3a2c67317e9d9ebb79889d2dcd2b6520
Silbye <silverxlxl9@gmail.com>
2022-08-11 04:23:58 +0300

    added weekend finder

---
+++ lesson2/hometasks/task3/Program.cs
@@ -0,0 +1,24 @@
+﻿void weekendFinder()
+{
+    Console.Clear();
+
+    Console.Write("Enter your day: ");
+    int day = Convert.ToInt32(Console.ReadLine());
+
+    if (day > 0 & day < 8)
+    {
+        if (day == 6 || day == 7)
+        {
+            Console.WriteLine("Your day is weekend!");
+        }
+        else
+        {
+            Console.WriteLine("Your day is NOT weekend!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("There is no such day in the week!");
+    }
+}
+weekendFinder();
