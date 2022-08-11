b5015550adc7e2baee5f89d2934fc9e0b043d507
Silbye <silverxlxl9@gmail.com>
2022-08-11 04:00:05 +0300

    added second digit of number finder

---
+++ lesson2/hometasks/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿void findSecondDigit()
+{
+    Console.Clear();
+
+    Console.Write("Enter your Number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int secondDigit = 0;
+
+    if (number > 100 & number < 1000)
+    {
+        secondDigit = number % 100;
+        secondDigit/=10;
+        Console.WriteLine("The second digit of your number is - " + secondDigit);
+    }
+    else
+    {
+        Console.WriteLine("Your number does NOT consist of 3 digits");
+    }
+}
+findSecondDigit();
