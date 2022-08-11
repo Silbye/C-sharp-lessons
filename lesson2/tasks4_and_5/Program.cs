9d89159ba022781dcb259fcdddb7c87ed892c0b1
Silbye <silverxlxl9@gmail.com>
2022-08-11 04:17:54 +0300

    '

--- lesson2/tasks4_and_5/Program.cs
+++ lesson2/tasks4_and_5/Program.cs
@@ -5,12 +5,10 @@
     Console.Write("Enter your Number: ");
     int number = Convert.ToInt32(Console.ReadLine());
 
-    if(number % 7 == 0 & number % 23 == 0)
-    {
+    if(number % 7 == 0 & number % 23 == 0){
         Console.WriteLine("Your number IS a multiple of both 7 and 23");
     }
-    else
-    {
+    else{
         Console.WriteLine("Your number IS NOT a multiple of both 7 and 23");
     }
 }
@@ -21,18 +19,14 @@ void findSquare()
     Console.Write("Enter Number 2: ");
     int number2 = Convert.ToInt32(Console.ReadLine());
 
-    if(number1 / number2 == number2)
-    {
+    if(number1 / number2 == number2){
         Console.WriteLine("Number 1 is square of Number 2");
     }
-    else
-    {
-        if(number2 / number1 == number1)
-        {
+    else{
+        if(number2 / number1 == number1){
             Console.WriteLine("Number 2 is square of Number 1");
         }
-        else
-        {
+        else{
             Console.WriteLine("Neither Number is a square of the other");
         }
     }
