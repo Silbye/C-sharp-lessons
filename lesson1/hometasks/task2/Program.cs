9fd43a4fc158a68c9c7d03849aafdcc004c80fad
Silbye <silverxlxl9@gmail.com>
2022-08-11 19:24:27 +0300

    optimized max number out of 3 program

--- lesson1/hometasks/task2/Program.cs
+++ lesson1/hometasks/task2/Program.cs
@@ -12,17 +12,16 @@ int number3 = Convert.ToInt32(Console.ReadLine());
 
 int max = number1;
 
-if(number2 > max)
+if (number2 > number3)
 {
-    max = number2;
-    if(number3>max)
+    if (number2 > max)
     {
-        max = number3;
+        max = number2;
     }
 }
 else
 {
-    if(number3>max)
+    if (number3 > max)
     {
         max = number3;
     }
