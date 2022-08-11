37c89077cbbd65cd021ed675739835b10c2d8198
Silbye <silverxlxl9@gmail.com>
2022-08-11 04:16:37 +0300

    added find third digit method

---
+++ lesson2/hometasks/task2/Program.cs
@@ -0,0 +1,18 @@
+﻿void findThirdDigit()
+{
+    Console.Clear();
+
+    Console.Write("Enter your Number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    string thirdDigit = number.ToString();
+
+    if (number > 100)
+    {
+        Console.WriteLine("The third digit of your number is - " + thirdDigit[2]);
+    }
+    else
+    {
+        Console.WriteLine("Your number does NOT have at least 3 digits");
+    }
+}
+findThirdDigit();
