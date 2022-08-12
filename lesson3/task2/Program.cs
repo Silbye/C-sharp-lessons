c9e345d5a6b9f2aa1bef2f45d7eeb34bfce8af0a
Silbye <silverxlxl9@gmail.com>
2022-08-12 20:34:42 +0300

    added x and y finder from quarter

---
+++ lesson3/task2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.Write("Enter your Quarter: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+string result = findGraph(quarter);
+Console.WriteLine(result);
+
+string findGraph(int quarter)
+{
+    if (quarter == 1)
+    {
+        return "x > 0, y > 0";
+    }
+    if (quarter == 2)
+    {
+        return "x < 0, y > 0";
+    }    
+    if (quarter == 3)
+    {
+        return "x < 0, y < 0";
+    }  
+    if (quarter == 4)
+    {
+        return "x > 0, y < 0";
+    }  
+    throw new Exception("There is no such Quarter");
+}
