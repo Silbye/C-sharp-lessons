a1ff90a7e2fec240ed5eab763f4ba516d6832bac
Silbye <silverxlxl9@gmail.com>
2022-08-12 20:23:09 +0300

    added graph quarter finder

---
+++ lesson3/task1/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+Console.Write("Enter X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+int result = findGraphQuarter(x, y);
+Console.WriteLine("Your quarter is - " + result);
+
+int findGraphQuarter(int x, int y)
+{
+    if (x > 0)
+    {
+        if (y > 0)
+        {
+            return 1;
+        }
+        if (y < 0)
+        {
+            return 4;
+        }
+    }
+    if (x < 0)
+    {
+        if (y > 0)
+        {
+            return 2;
+        }
+        if (y < 0)
+        {
+            return 3;
+        }
+    }
+    throw new Exception("Numbers cannot be 0");
+}
