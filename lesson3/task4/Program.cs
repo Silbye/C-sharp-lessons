30ba8b23a8877d8b5383e019dda81f9d1cf044a2
Silbye <silverxlxl9@gmail.com>
2022-08-12 21:21:51 +0300

    added all squares of a number display

---
+++ lesson3/task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Enter your number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int number = 0;
+string result = "";
+
+string getSquares(int N){
+    while(number <= N){
+        result+=Math.Pow(N, number)+", ";
+        number++;
+    }
+    return result;
+}
+
+string display = getSquares(N);
+Console.WriteLine(display);
