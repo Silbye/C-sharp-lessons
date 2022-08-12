d71667b3cb29dad31535d3bce7d962c1867bc18e
Silbye <silverxlxl9@gmail.com>
2022-08-12 21:43:59 +0300

    fixed sqares finder

--- lesson3/task4/Program.cs
+++ lesson3/task4/Program.cs
@@ -1,12 +1,12 @@
 ﻿Console.Clear();
 Console.Write("Enter your number: ");
 int N = Convert.ToInt32(Console.ReadLine());
-int number = 0;
+int number = 1;
 string result = "";
 
 string getSquares(int N){
-    for(int i=0;i<=N;i++){
-        result+=Math.Pow(N, i)+", ";
+    for(int i=1;i<=N;i++){
+        result+=Math.Pow(i, 2)+", ";
     }
     //while(number <= N){
         //result+=Math.Pow(N, number)+", ";
