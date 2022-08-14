e1ef3ccada30653fffedcc1c21f1f72f83b57429
Silbye <silverxlxl9@gmail.com>
2022-08-14 06:40:10 +0300

    added square 3 list

---
+++ lesson3/hometasks/task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Enter your number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+string result = "";
+
+string getSquares(int N){
+    for(int i=1;i<=N;i++){
+        result+=Math.Pow(i, 3)+", ";
+    }
+    return result;
+}
+
+string display = getSquares(N);
+Console.WriteLine(display);
