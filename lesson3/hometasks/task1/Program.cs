a6e18b92726a817b3af4914c4ae32efe823e4728
Silbye <silverxlxl9@gmail.com>
2022-08-14 06:41:04 +0300

    added palindrom check

---
+++ lesson3/hometasks/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Enter your number: ");
+string Number = Console.ReadLine();
+
+bool checkPalindrom(string Number){
+    if(Number.Reverse().SequenceEqual(Number)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool display = checkPalindrom(Number);
+Console.WriteLine(display);
