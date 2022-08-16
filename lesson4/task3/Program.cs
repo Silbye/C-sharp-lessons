209638a0d73c3403d2389a5798773ceeae787f20
Silbye <silverxlxl9@gmail.com>
2022-08-16 22:14:07 +0300

    added random array displayer

---
+++ lesson4/task3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+int[] array = new int[8];
+
+string PrintArray(int[] array)
+{
+    int count = array.Length;
+    int index = 0;
+    string result = "";
+    while (index < count)
+    {
+        array[index] = new Random().Next(0, 2);
+        result+=array[index]+", ";
+        index++;
+    }
+    return result;
+}
+
+string display = PrintArray(array);
+Console.WriteLine(display);
