e8fee73da3eb0c50fe299df55cc1565f4868fdc0
Silbye <silverxlxl9@gmail.com>
2022-08-16 22:12:49 +0300

    added digit count in number

---
+++ lesson4/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.Write("Enter your number: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int Count = 0;
+
+int digitsAmountFinder(int Number)
+{
+    if (Number == 0)
+    {
+        Count = 1;
+    }
+    else
+    {
+        while (Number > 0)
+        {
+            Number /= 10;
+            Count++;
+        }
+    }
+    return Count;
+}
+
+int display = digitsAmountFinder(Number);
+Console.WriteLine(display);
