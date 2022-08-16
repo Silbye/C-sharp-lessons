df612abb511cb775f8e8996bac2b3c3cd2fbaa03
Silbye <silverxlxl9@gmail.com>
2022-08-16 22:13:46 +0300

    added factorial finder

---
+++ lesson4/task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Enter your number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int Count = 1;
+
+string getFactorial(int N){
+    string result = "";
+    for(int i=1;i<=N;i++){
+        Count=Count*i;
+        result=result+Count+", ";
+    }
+    return result;
+}
+
+string display = getFactorial(N);
+Console.WriteLine(display);
