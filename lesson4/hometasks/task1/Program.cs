3b7de1470a8eb59f88e5f134d851a4538ca77f1e
Silbye <silverxlxl9@gmail.com>
2022-08-19 01:10:08 +0300

    added square of 2 numbers finder

---
+++ lesson4/hometasks/task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Enter number A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+double getSquare(int A, int B){
+    double result = Math.Pow(A, B);
+    return result;
+}
+
+double display = getSquare(A, B);
+Console.WriteLine(display);
