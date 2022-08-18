97089fa240e29813a8e76dbb82c7a68e39dfa270
Silbye <silverxlxl9@gmail.com>
2022-08-19 01:09:04 +0300

    moved result variable into method

--- lesson3/hometasks/task3/Program.cs
+++ lesson3/hometasks/task3/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.Clear();
 Console.Write("Enter your number: ");
 int N = Convert.ToInt32(Console.ReadLine());
-string result = "";
 
 string getSquares(int N){
+    string result = "";
     for(int i=1;i<=N;i++){
         result+=Math.Pow(i, 3)+", ";
     }
