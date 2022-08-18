feaf795d1d96118e18f92e8dd4fa01c968d8500e
Silbye <silverxlxl9@gmail.com>
2022-08-19 01:10:35 +0300

    added digits of a number summ

---
+++ lesson4/hometasks/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.Write("Enter your Number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int findDigitSumm(int number){
+    int summ = 0;
+    if(number >= 10){
+        while(number>0){
+            summ+=number%10;
+            number/=10;
+        }
+    }
+    else{
+        summ = number;
+    }
+    return summ;
+}
+
+int display = findDigitSumm(number);
+Console.WriteLine(display);
