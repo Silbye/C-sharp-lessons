847dd5e99b06b7587ead7bc89cef4e2f753b2baa
Silbye <silverxlxl9@gmail.com>
2022-08-19 22:19:58 +0300

    added array without negative numbers

---
+++ lesson5/task1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+int[] array = new int[4];
+
+string PrintArrayWithMinusNumbers(int[] array)
+{
+    int amount = array.Length;
+    string result = "";
+    for(int i = 0; i<array.Length; i++){
+        array[i] = new Random().Next(-8, 9);
+        result+=array[i]+" ";
+    }
+    return result;
+}
+
+string PrintArray(int[] array)
+{
+    string result = "";
+    for(int i = 0; i<array.Length; i++){
+        if(array[i]<0){
+          array[i]=-array[i];
+        }
+        result+=array[i]+" ";
+    }
+    return result;
+}
+
+string display = PrintArrayWithMinusNumbers(array);
+string display2 = PrintArray(array);
+Console.WriteLine(display);
+Console.WriteLine(display2);
