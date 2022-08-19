2d16c9bc3d5ce520d187a12b47b528e94415c602
Silbye <silverxlxl9@gmail.com>
2022-08-19 22:20:47 +0300

    added numbers in range in array counter

---
+++ lesson5/task3/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+int[] array = new int[123];
+
+string PrintArray(int[] array){
+    string result = "";
+    for(int i = 0; i<array.Length; i++){
+        array[i] = new Random().Next(1, 124);
+        result+=array[i]+" ";
+    }
+    return result;
+}
+
+int FindNumberInRangeAmount(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i<array.Length; i++){
+        if(array[i] >= 10 && array[i] <= 99){
+          count++;
+        }
+    }
+    return count;
+}
+string display = PrintArray(array);
+int display2 = FindNumberInRangeAmount(array);
+Console.WriteLine(display);
+Console.WriteLine(display2);
