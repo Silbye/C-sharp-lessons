3b131691c80b2a8a4d41aea427b7ca9375028e62
Silbye <silverxlxl9@gmail.com>
2022-08-21 20:43:07 +0300

    added uneven index numbers summ

---
+++ lesson5/hometasks/task2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+int[] array = new int[10];
+
+string PrintArray(int[] array){
+    string result = "";
+    for(int i = 0; i<array.Length; i++){
+        array[i] = new Random().Next(-100, 101);
+        result+=array[i]+" ";
+    }
+    return result;
+}
+
+int FindUnevenIndexNumberSumm(int[] array)
+{
+    int summ = 0;
+    for(int i = 0; i<array.Length; i++){
+        if(i % 2 == 1){
+            summ+=array[i];
+        }
+    }
+    return summ;
+}
+string display = PrintArray(array);
+int display2 = FindUnevenIndexNumberSumm(array);
+Console.WriteLine(display);
+Console.WriteLine(display2);
