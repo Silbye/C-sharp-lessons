d3d9b4d1d07fa5ee95e9b9ce0ecb133aab2fa581
Silbye <silverxlxl9@gmail.com>
2022-08-21 20:42:43 +0300

    added even 3 digit number amount finder

---
+++ lesson5/hometasks/task1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+int[] array = new int[10];
+
+string PrintArray(int[] array){
+    string result = "";
+    for(int i = 0; i<array.Length; i++){
+        array[i] = new Random().Next(100, 1000);
+        result+=array[i]+" ";
+    }
+    return result;
+}
+
+int FindEvenNumberAmount(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i<array.Length; i++){
+        if(array[i] % 2 == 0){
+          count++;
+        }
+    }
+    return count;
+}
+string display = PrintArray(array);
+int display2 = FindEvenNumberAmount(array);
+Console.WriteLine(display);
+Console.WriteLine(display2);
