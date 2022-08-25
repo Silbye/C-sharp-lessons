2799831d79bf31b8ad2cc528f402b3ad2505a793
Silbye <silverxlxl9@gmail.com>
2022-08-25 15:53:51 +0300

    added fibonacci number finder without recursion

---
+++ lesson6/task3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+int number0 = 0;
+int number1 = 1;
+int number2;
+Console.Write("Enter your number: ");
+int neededNumber = Convert.ToInt32(Console.ReadLine());
+
+string Fibonacci(int n){
+    string result = number0 + " " + number1 + " ";
+    for(int i = 3; i <= n; i++){
+        number2 = number0 + number1;
+        result += number2+" ";
+        number0 = number1;
+        number1 = number2;
+    }
+    return result;
+}
+
+string display = Fibonacci(neededNumber);
+Console.WriteLine(display);
