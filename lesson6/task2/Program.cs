c67d0a89e0cafabc1a2d3cd2bd0c41131424aab2
Silbye <silverxlxl9@gmail.com>
2022-08-25 15:53:29 +0300

    added converter to binary numbers

---
+++ lesson6/task2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.Write("Enter your number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int ConvertToBinary(int N)
+{
+    int a = 0;
+    string binaryNumber = "";
+    if (N == 2)
+    {
+        binaryNumber = "10";
+    }
+    else
+    {
+        while (N >= 1)
+        {
+            a = N % 2;
+            binaryNumber += a;
+            N /= 2;
+        }
+    }
+    int newNumber = Int32.Parse(binaryNumber);
+    return newNumber;
+}
+
+int display = ConvertToBinary(N);
+Console.WriteLine(display);
