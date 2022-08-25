7dbd9acf1fbb1dc27b827c7dd01d5633a17a17a6
Silbye <silverxlxl9@gmail.com>
2022-08-25 15:54:24 +0300

    added singular array copier

---
+++ lesson6/task4/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+int[] array = new int[10];
+
+string PrintArray(int[] array){
+    string result = "";
+    for(int i = 0; i<array.Length; i++){
+        Console.Write("Enter your array member: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        result+=array[i]+" ";
+    }
+    return result;
+}
+
+string CopyArray(int[] array){
+    int[] arrayCopy = new int[10];
+    string result = "";
+    for(int i = 0; i < arrayCopy.Length; i++){
+        arrayCopy[i] = array[i];
+        result+=arrayCopy[i]+" ";
+    }
+    return result;
+}
+
+string method1 = PrintArray(array);
+string method2 = CopyArray(array);
+Console.WriteLine("Your array is: " + method1);
+Console.WriteLine("Your array copy is: " + method2);
