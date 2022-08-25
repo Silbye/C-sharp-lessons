301568587952f62d9efc944b04409d37b51e45eb
Silbye <silverxlxl9@gmail.com>
2022-08-25 15:51:27 +0300

    added positive numbers amount finder

---
+++ lesson6/hometasks/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Enter your numbers (use a space to divide numbers): ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int FindPositiveNumbersAmount(int[] array){
+    int count = 0;
+    for(int i = 0; i < array.Length; i++){
+        if(array[i] > 0)
+        {
+            count++;
+        }        
+    }
+    return count;
+}
+
+int display = FindPositiveNumbersAmount(array);
+Console.WriteLine("Positive numbers amount: " + display);
