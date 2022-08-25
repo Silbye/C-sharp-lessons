caec0b23987ce6cccd603aff6cb294b6512ec53c
Silbye <silverxlxl9@gmail.com>
2022-08-25 15:53:13 +0300

    added checker if a triangle can exist

---
+++ lesson6/task1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.Write("Enter number a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+bool CanTriangleExist(int a, int b, int c){
+    bool triangle = false;
+    if(a<b+c){
+        if(b<a+c){
+            if(c<a+b){
+                triangle = true;
+            }
+        }
+    }
+    else{
+        triangle = false;
+    }
+    return triangle;
+}
+bool display = CanTriangleExist(a,b,c);
+if(display == true){
+    Console.WriteLine("Your triangle can exist");
+}
+else{
+    Console.WriteLine("Your triangle can not exist");
+}
