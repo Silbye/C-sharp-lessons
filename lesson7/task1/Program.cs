a6fd132533ea34a1e8cde0e348d9d35d81f5f8d0
Silbye <silverxlxl9@gmail.com>
2022-08-26 20:33:31 +0300

    added double array with index summs maker

---
+++ lesson7/task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+void PrintArray(int[,] array){
+    for(int i = 0; i<array.GetLength(0); i++){
+        for(int j = 0; j<array.GetLength(1); j++){
+            array[i, j] = i+j;
+            Console.Write($"{array[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+int[,] array = new int[3, 4];
+PrintArray(array);
