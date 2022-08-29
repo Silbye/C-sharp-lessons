b51aed03a529d6868bdc5bae0af832ad97826c5f
Silbye <silverxlxl9@gmail.com>
2022-08-29 19:23:58 +0300

    added index of number in double array finder

---
+++ lesson7/hometasks/task2/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+
+void PrintArray(int[,] array){
+    for(int i = 0; i<array.GetLength(0); i++){
+        for(int j = 0; j<array.GetLength(1); j++){
+            array[i, j] = new Random().Next(1, 10);
+            Console.Write($"{array[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+int[,] array = new int[3, 4];
+PrintArray(array);
+
+Console.Write("Enter number you want to find: ");
+int neededNumber = Convert.ToInt32(Console.ReadLine());
+
+void FindNumber(int[,] array){
+    bool numberFound = false;
+    for(int i = 0; i<array.GetLength(0); i++){
+        for(int j = 0; j<array.GetLength(1); j++){
+            if(array[i, j] == neededNumber){
+                numberFound = true;
+                Console.Write($"{i}; {j}");
+                Console.WriteLine();
+            }
+        }
+    }
+    if (numberFound == true){
+        Console.WriteLine("Number found!");
+    }
+    if (numberFound == false){
+        Console.WriteLine("Number not found!");
+    }
+}
+
+FindNumber(array);
