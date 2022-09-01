0e3ff4c19dd470448fe1bd921ac1d6d32a278dde
Silbye <silverxlxl9@gmail.com>
2022-09-02 00:08:17 +0300

    made a display array method

--- lesson7/hometasks/task2/Program.cs
+++ lesson7/hometasks/task2/Program.cs
@@ -1,38 +1,66 @@
 ﻿Console.Clear();
 
-void PrintArray(int[,] array){
+int[] DetermineArraySize(){
+    int[] size = new int[2];
+    for(int i=0; i<size.Length; i++){
+        Console.WriteLine("Enter your array size: ");
+        size[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return size;
+}
+
+void CreateArray(int[,] array){
     for(int i = 0; i<array.GetLength(0); i++){
         for(int j = 0; j<array.GetLength(1); j++){
             array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array){
+    for(int i = 0; i<array.GetLength(0); i++){
+        for(int j = 0; j<array.GetLength(1); j++){
             Console.Write($"{array[i, j]} ");
         }
     Console.WriteLine();
     }
 }
 
-int[,] array = new int[3, 4];
+int[] size = DetermineArraySize();
+int[,] array = new int[size[0], size[1]];
+CreateArray(array);
 PrintArray(array);
+Console.WriteLine();
 
 Console.Write("Enter number you want to find: ");
 int neededNumber = Convert.ToInt32(Console.ReadLine());
 
-void FindNumber(int[,] array){
-    bool numberFound = false;
+int[] FindNumber(int[,] array){
+    int[] index = new int[2];
+    bool foundNumber = false;
     for(int i = 0; i<array.GetLength(0); i++){
         for(int j = 0; j<array.GetLength(1); j++){
             if(array[i, j] == neededNumber){
-                numberFound = true;
-                Console.Write($"{i}; {j}");
-                Console.WriteLine();
+                foundNumber = true;
+                index[0] = i;
+                index[1] = j;
             }
         }
     }
-    if (numberFound == true){
-        Console.WriteLine("Number found!");
+    if(foundNumber == true){
+        return index;
     }
-    if (numberFound == false){
-        Console.WriteLine("Number not found!");
+    else{
+        throw new Exception("Couldn't find your number");
     }
 }
 
-FindNumber(array);
+int[] numberIndex = new int[2];
+numberIndex = FindNumber(array);
+
+void PrintFoundIndex(int[] array){
+    for(int i=0; i<array.Length; i++){
+        Console.Write($"{array[i]} ");
+    }
+}
+PrintFoundIndex(numberIndex);
