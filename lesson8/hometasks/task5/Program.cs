ac2d07ff8d56e7cc917a59c72947d18f6de48f0b
Silbye <silverxlxl9@gmail.com>
2022-09-02 00:10:52 +0300

    made spiral filled array

---
+++ lesson8/hometasks/task5/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+void CreateArray(int[,] array){
+    int i = 0;
+    int j = 0;
+    for(int input = 1; input <= array.GetLength(0)*array.GetLength(1); input++){
+        array[i,j] = input;
+        if (i <= j + 1 && i + j < array.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= array.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > array.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+}
+
+void PrintArray(int[,] array){
+    for(int i = 0; i<array.GetLength(0); i++){
+        for(int j = 0; j<array.GetLength(1); j++){
+            Console.Write($"{array[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+int[,] array = new int[4, 4];
+CreateArray(array);
+PrintArray(array);
+Console.WriteLine();
