37a0493db1b0604b665f6e34ed1f078c4313320d
Silbye <silverxlxl9@gmail.com>
2022-09-06 11:18:13 +0300

    made find summ from m to n via recursion

---
+++ lesson9/hometasks/task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Enter M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int summ = 0;
+
+int FindSumm(int numberM, int numberN){
+    if (numberM <= numberN){
+        summ+=numberM;
+        numberM++;
+        FindSumm(numberM, numberN);
+    }
+    return summ;
+}
+Console.WriteLine(FindSumm(numberM, numberN));
