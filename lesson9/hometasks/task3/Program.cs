3f2bc60c1d8947c49760b7aff0e2e3515a00162e
Silbye <silverxlxl9@gmail.com>
2022-09-06 11:18:50 +0300

    made ackermann function finder via recursion

---
+++ lesson9/hometasks/task3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Enter M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int FindAckermann(int numberM, int numberN){
+    if (numberM == 0) return numberN + 1;
+    else{
+        if((numberM != 0) && (numberN == 0)){
+            return FindAckermann(numberM - 1, 1);
+        }
+        else return FindAckermann(numberM - 1, FindAckermann(numberM, numberN - 1));
+    }
+}
+Console.WriteLine(FindAckermann(numberM, numberN));
